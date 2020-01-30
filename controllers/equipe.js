c7cac5a57b947b9c3e25772379abc8055c849d37
fez <eugenefezeu@gmail.com>
2020-01-30 02:14:02 +0100

    stp

--- controllers/equipe.js
+++ controllers/equipe.js
@@ -199,12 +199,13 @@ exports.getByEdition = (req, res) => {
                 res.send(r);
             }
 
-            for await (let eq of getE()) {
-                var j = eq.toJSON();
-                j.joueurs = await Joueur.find({ equipe: eq._id }).populate(
-                    "image"
-                );
-                r.push(j);
-            }
+            //for await (let eq of getE()) {
+               // var j = eq.toJSON();
+                //j.joueurs = await Joueur.find({ equipe: eq._id }).populate(
+                 //   "image"
+              //  );
+               
+            //}
+            r.push(j);
         });
 };
